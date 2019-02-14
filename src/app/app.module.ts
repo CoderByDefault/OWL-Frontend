e08c85fc8853fbd73765f3d30ec80ee52e47c392
Arsalan Gul <gul@mail.uni-paderborn.de>
2019-02-14 23:45:42 +0100

    Front End commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventItemComponent } from './event-item/event-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
